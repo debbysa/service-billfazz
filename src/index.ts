d65183b88068d7b5eb17bcc10140fc0f6330000d
debbysa <deby.silvia23@gmail.com>
2021-01-08 10:38:54 +0700

    feat(classes): add advice billfazz

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import { Config } from './classes/Config'
+import { DepositeBillFazz } from './classes/Deposit-billfazz'
+import { ProductBillFazz } from './classes/Product-billfazz'
+import { ProductType } from './interfaces/enum'
 
-export { Config }
+export {
+  Config,
+  DepositeBillFazz,
+  ProductBillFazz,
+  // interface
+  ProductType,
+}
